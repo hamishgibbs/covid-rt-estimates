e2803a963d19fd2bceb0c0535dd6cebce173251b
Joe Hickson <joseph.hickson@metoffice.gov.uk>
2020-08-25 14:32:14 +0100

    restructure into functions, make more defensive to ensure it can continue, capture errors to logger (resolves #14)

--- R/run-region-updates.R
+++ R/run-region-updates.R
@@ -13,44 +13,78 @@ source(here::here("R", "update-regional.R"))
 # load utils
 source(here::here("R", "utils.R"))
 
-# set up the arguments
-option_list <- list(
-  make_option(c("-v", "--verbose"), action = "store_true", default = FALSE, help = "Print verbose output "),
-  make_option(c("-w", "--werbose"), action = "store_true", default = FALSE, help = "Print v.verbose output "),
-  make_option(c("-q", "--quiet"), action = "store_true", default = FALSE, help = "Print less output "),
-  make_option(c("--log"), type = "character", help = "Specify log file name"),
-  make_option(c("-e", "--exclude"), default = "", type = "character", help = "List of locations to exclude. See include for more details."),
-  make_option(c("-i", "--include"), default = "", type = "character", help = "List of locations to include (excluding all non-specified), comma separated in the format region/subregion or region/*. Case Insensitive. Spaces can be included using quotes - e.g. \"united-states/rhode island, United-States/New York\""),
-  make_option(c("-u", "--unstable"), action = "store_true", default = FALSE, help = "Include unstable locations"),
-  make_option(c("-f", "--force"), action = "store_true", default = FALSE, help = "Run even if data for a region has not been updated since the last run")
-)
+#' Run Regional Updates
+#'
+run_regional_updates <- function(regions, args) {
+  # validate and load configuration
+  if (nchar(args$exclude) > 0 && nchar(args$include) > 0) {
+    stop("not possible to both include and exclude regions / subregions")
+  }
+  excludes <- parse_cludes(args$exclude)
+  includes <- parse_cludes(args$include)
 
-args <- parse_args(OptionParser(option_list = option_list))
-
-setup_log_from_args(args)
-
-# handle include / exclude options
-if (nchar(args$exclude) > 0 && nchar(args$include) > 0) {
-  stop("not possible to both include and exclude regions / subregions")
+  # now really do something
+  rru_process_locations(regions, args, excludes, includes)
 }
-excludes <- parse_cludes(args$exclude)
-includes <- parse_cludes(args$include)
 
-for (location in regions) {
-  if (excludes[region == location$name & subregion == "*", .N] > 0) {
-    futile.logger::flog.debug("skipping location %s as it is in the exclude/* list", location$name)
-    next()
+rru_cli_interface <- function() {
+  # set up the arguments
+  option_list <- list(
+    make_option(c("-v", "--verbose"), action = "store_true", default = FALSE, help = "Print verbose output "),
+    make_option(c("-w", "--werbose"), action = "store_true", default = FALSE, help = "Print v.verbose output "),
+    make_option(c("-q", "--quiet"), action = "store_true", default = FALSE, help = "Print less output "),
+    make_option(c("--log"), type = "character", help = "Specify log file name"),
+    make_option(c("-e", "--exclude"), default = "", type = "character", help = "List of locations to exclude. See include for more details."),
+    make_option(c("-i", "--include"), default = "", type = "character", help = "List of locations to include (excluding all non-specified), comma separated in the format region/subregion or region/*. Case Insensitive. Spaces can be included using quotes - e.g. \"united-states/rhode island, United-States/New York\""),
+    make_option(c("-u", "--unstable"), action = "store_true", default = FALSE, help = "Include unstable locations"),
+    make_option(c("-f", "--force"), action = "store_true", default = FALSE, help = "Run even if data for a region has not been updated since the last run")
+  )
+
+  args <- parse_args(OptionParser(option_list = option_list))
+  return(args)
+}
+
+
+rru_process_locations <- function(regions, args, excludes, includes) {
+  for (location in regions) {
+    if (excludes[region == location$name & subregion == "*", .N] > 0) {
+      futile.logger::flog.debug("skipping location %s as it is in the exclude/* list", location$name)
+      next()
+    }
+    if (includes[, .N] > 0 && includes[region == location$name, .N] == 0) {
+      futile.logger::flog.debug("skipping location %s as it is not in the include list", location$name)
+      next()
+    }
+    if (location$stable || (exists("unstable", args) && args$unstable == TRUE)) {
+      trycatch({
+                 update_regional(location,
+                                 excludes[region == location$name],
+                                 includes[region == location$name],
+                                 args$force)
+               },
+               warning = function(w) {
+                 futile.logger::flog.warn(w)
+               },
+               error = function(e) {
+                 futile.logger::flog.error(e)
+               }
+      )
+    }else {
+      futile.logger::flog.debug("skipping location %s as unstable", location$name)
+    }
   }
-  if (includes[, .N] > 0 && includes[region == location$name, .N] == 0) {
-    futile.logger::flog.debug("skipping location %s as it is not in the include list", location$name)
-    next()
-  }
-  if (location$stable || (exists("unstable", args) && args$unstable == TRUE)) {
-    update_regional(location,
-                    excludes[region == location$name],
-                    includes[region == location$name],
-                    args$force)
-  }else {
-    futile.logger::flog.debug("skipping location %s as unstable", location$name)
-  }
+}
+
+# only execute if this is the root, passing in regions from region-list.R and the args from the cli interface
+# this bit handles the outer logging wrapping and top level error handling
+if (sys.nframe() == 0) {
+  args <- rru_cli_interface()
+  setup_log_from_args(args)
+  tryCatch(run_regional_updates(regions = regions, args = args),
+           warning = function(w) {
+             futile.logger::flog.warn(w)
+           },
+           error = function(e) {
+             futile.logger::flog.error(e)
+           })
 }
